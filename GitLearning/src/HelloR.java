1768d14a19b7336963c18f482cf5bb2f6cf66dfa
rosha <rosha@192.168.2.30>
2018-07-21 14:45:10 +0530

    Roshan has changed the sysout statement

--- GitLearning/src/HelloR.java
+++ GitLearning/src/HelloR.java
@@ -2,7 +2,7 @@
 public class HelloR {
 
 	public void show(){
-		System.out.println("Welcome");
+		System.out.println("Welcome Roshan Mohapatra");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
