b8f6ed83a930d76789eba70fa3036208f5b5d7a4
ManishP <ManishP@DESKTOP-AI1BJKH>
2018-07-21 14:08:22 +0530

    first commit!

---
+++ GitLearning/src/HelloR.java
@@ -0,0 +1,13 @@
+
+public class HelloR {
+
+	public void show(){
+		System.out.println("Welcome");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+HelloR hello = new HelloR();
+hello.show();
+	}
+
+}
